f2c6bbfb977ae750e746522fe5a920ef601de815
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-08-15 15:54:19 +0200

    Add adjustments

--- docs/js/slider.js
+++ docs/js/slider.js
@@ -9,7 +9,7 @@
       alt: 'Slider image two'
     },
     {
-      src: 'https://cdn.pixabay.com/photo/2016/02/19/10/55/light-bulb-1209491_1280.jpg',
+      src: 'https://cdn.pixabay.com/photo/2017/12/13/11/02/light-bulb-3016538_1280.jpg',
       alt: 'Slider image three'
     },
   ];
